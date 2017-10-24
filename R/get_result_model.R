8a832415f4ccbdbb279555d2f70e3b447226ce70
gaelleVF <gaelle.van.frank@gmail.com>
2017-10-24 10:41:54 +0200

    change way to specify year

--- R/get_result_model.R
+++ R/get_result_model.R
@@ -54,7 +54,8 @@ get_result_model = function(res_model, data, type_result = "comparison", variabl
 	germplasm = unlist(lapply(as.character(noms),function(x){strsplit(x,"_")[[1]][1]}))
 	env = unlist(lapply(as.character(noms),function(x){strsplit(x,"_")[[1]][2]}))
 	block = data$block
-	if(is.null(year)){year = unlist(lapply(as.character(noms),function(x){strsplit(x,"_")[[1]][3]}))}
+	year_to_keep = year
+	year = unlist(lapply(as.character(noms),function(x){strsplit(x,"_")[[1]][3]}))
   
 	if (type_result == "comparison") {	
 	  comp.param = paste("comp",param,sep=".")
@@ -83,6 +84,7 @@ get_result_model = function(res_model, data, type_result = "comparison", variabl
 	}
 	
 	if (!is.null(ID)) { colnames(ID) = c("ID","parameter")}
+	if(!is.null(year_to_keep)){ID = ID[grep(paste(year_to_keep,collapse="|"), ID$parameter),]}
 	
 	
 # 3. Get model results -------------
