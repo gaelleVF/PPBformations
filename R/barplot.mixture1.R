cdf73e5e940f7cf02116debd639d5c38badb0487
gaelleVF <gaelle.van.frank@gmail.com>
2017-11-16 14:37:54 +0100

    more explicit legend

--- R/barplot.mixture1.R
+++ R/barplot.mixture1.R
@@ -14,10 +14,13 @@
 
 barplot.mixture1 = function(x, title, melange) {
   Gain = round((x[grep(paste("^",melange,"$",sep=""),x$entry),"median"]/x[grep("MoyenneComposantes",x$entry),"median"]-1)*100,2)
-  x$type = factor(x$type, levels=c("Composante","MoyenneComposantes","Mélange","Mélange Mod1","Mélange Mod 2","Mélange Mod 3"))
+  x$type = factor(x$type, levels=c("Composante","MoyenneComposantes","Mélange non sélectionné","Mélange issu 2 années sélection 
+  dans composantes (Mod1)","Mélange issu 1 année sélection 
+dans composantes (Mod2)","Mélange sélectionné (Mod3)"))
   
   p = ggplot(x, aes(x = reorder(germplasm, median), y = median, fill=unlist(x$type))) + geom_bar(stat = "identity")+ theme(legend.title = element_blank())
-  p = p + scale_fill_manual(drop=FALSE,values =c("olivedrab3","green4","red","springgreen3","gold1","skyblue1"))
+  p = p + scale_fill_manual(drop=TRUE,values =c("Composante"="olivedrab3","MoyenneComposantes"="green4","Mélange non sélectionné"="red","Mélange issu 2 années sélection \ndans composantes (Mod1)"="springgreen3",
+"Mélange issu 1 année sélection \ndans composantes (Mod2)"="gold1","Mélange sélectionné (Mod3)"="skyblue1"))
   
   # ajouter les groupes de significativité
   p = p + geom_text(data = x, aes(x = reorder(germplasm, median), y = median/2, label = groups), angle = 90, color = "white")
