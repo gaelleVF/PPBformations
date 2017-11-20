c33b3d6289a40f501a3cec9e8af9a533b84c765a
gaelleVF <gaelle.van.frank@gmail.com>
2017-11-20 16:50:08 +0100

    correct for automatization

--- R/get_mixture_tables.R
+++ R/get_mixture_tables.R
@@ -155,10 +155,16 @@ if(table.type == "distribution"){
     }
     d = read.table(paste(path_to_tables,"/Distrib_",i,".csv",sep=""),header=T,sep=";",comment.char="")
     if(!is.null(mix_to_delete)){d = d[-grep(paste(mix_to_delete,collapse="|"),d$melange),]}
-    tmp=cbind(as.numeric(as.character(d[seq(1,nrow(d),4),"Moyenne"])),as.numeric(as.character(d[seq(2,nrow(d),4),"Moyenne"])),as.numeric(as.character(d[seq(3,nrow(d),4),"Moyenne"])),as.numeric(as.character(d[seq(4,nrow(d),4),"Moyenne"])),as.numeric(as.character(d[seq(1,nrow(d),4),"pvalue"])))
+    tmp=cbind(as.numeric(as.character(d[grep("Worst",d$parameter),"Moyenne"])),
+              as.numeric(as.character(d[grep("MoyenneComposantes",d$parameter),"Moyenne"])),
+              as.numeric(as.character(d[grep("Mélange",d$parameter),"Moyenne"])),
+              as.numeric(as.character(d[grep("Best",d$parameter),"Moyenne"])),
+              as.numeric(as.character(d[seq(1,nrow(d),4),"pval"])))
+    
+    
     tmp = cbind(tmp,unlist(as.character(d$melange[seq(1,nrow(d),4)])),unlist(as.character(d$year[seq(1,nrow(d),4)])),unlist(as.character(d$location[seq(1,nrow(d),4)])))
     tmp=data.frame(tmp,stringsAsFactors = F)
-    colnames(tmp)=c(as.character(d[1:4,"Type"]),"pvalue_overY","melange","year","location")
+    colnames(tmp)=c("1.moins bonne","2.moyenne composantes","3.mélange","4.meilleure","pvalue_overY","melange","year","location")
     D=c(D,list(tmp))
   }
   names(D)=vec_variables
